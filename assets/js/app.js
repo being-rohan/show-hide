f6110d3e120abcfe1bf03e2fba9d1ec5131be563
rohan panchal <rohanpanchal216@gmail.com>
2023-12-14 20:04:52 +0530

    push

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,72 @@
- var cl= console.log;
-const passwordField = document.getElementById('password');
+const cl=console.log;
+
+const btn = document.getElementById("btn");
+const box = document.getElementById("box");
+
+//------------1--------------
+// let jack = true;
+// const hideBox = function(){
+// if(jack){
+//     cl(this);
+//          this.innerHTML = "Show Box";
+//        // box.classList.add("d-none")
+//         box.classList.toggle("d-none")
+//         //jack = false;
+//         jack = !jack;
+// }else{
+//     this.innerHTML = "Hide Box"
+//    // box.classList.remove("d-none")
+//     box.classList.toggle("d-none")
+//   //  jack = true;
+//     jack = !jack;
+// }
+   
+// }
+
+//----------2----------
+// let jack = true;
+// const hideBox = function(){
+// if(jack){
+//          this.innerHTML = "Show Box";
+// }else{
+//     this.innerHTML = "Hide Box"
+// }
+// box.classList.toggle("d-none")
+// jack = !jack;
+// }
+   
+//---------------3---------------
+let jack = true;
+const hideBox =function(){
+    jack ? this.innerHTML = "Show Box" : this.innerHTML = "Hide Box";
+    box.classList.toggle("d-none");
+    jack = !jack;
+}
+btn.addEventListener("click" , hideBox)
+
+//###################EVENT>TARGET###############################
 
 
+const btn1 = document.getElementById("btn1");
+const box1 = document.getElementById("box1");
 
-passwordField.addEventListener('click',handler)
+//--------1-----------
+// const toggleBox = eve => {
+//    if(jack){
+//     eve.target.innerHTML = "Show Box"
+//    }else{
+//     eve.target.innerHTML= "Hide Box"
+//    }
+//    box1.classList.toggle("d-none");
+//    jack = !jack;
+// }
 
-let handler =(hand)=>{
-    if (hand.checked) {
-        // If checkbox is checked, show the password
-        hand.type = 'text';
-    } else {
-        // If checkbox is unchecked, hide the password
-        hand.type = 'password';
-    }
-};
 
-cl(handler)
+//---------2--------
+const toggleBox = eve =>{
+    jack ? eve.target.innerHTML = "Show Box" : eve.target.innerHTML ="Hide Box";
+    box1.classList.toggle("d-none");
+    jack = !jack;
+}
+
+btn1.addEventListener("click", toggleBox)
